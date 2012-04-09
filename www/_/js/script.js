6ccd186d78a2add3f82b3c166391867499ca35de
John Polacek <johnpolacek@gmail.com>
2012-04-09 14:18:28 -0500

    linting updates, updated comments

--- www/_/js/script.js
+++ www/_/js/script.js
@@ -1,26 +1,26 @@
 var App = (function() {
     
-    // Keep vars & functions private unless there is a reason to use in other 
+    // Keep vars & functions private unless there is a reason to use in other
     // scripts running on the page. For example, to let another script
     // use getCookie(), move it inside the returned object closure.
     
     // Private Vars
     
-    var appData = {};
+    var appData = {};  // note: Recommend keeping data in DOM when appropriate
     
     
     // Private Functions
     
     //--------------------------------------
     //  EVENT HANDLING
-    //--------------------------------------    
+    //--------------------------------------
     
     function setEvents() {
-        $('.main-nav').on("click", {message: 'You just clicked '}, mainNavHandler)
+        $('.main-nav').on("click", {message: 'You just clicked '}, mainNavHandler);
     }
     
-    function mainNavHandler(event) {
-        event.preventDefault();
+    function mainNavHandler(e) {
+        e.preventDefault();
         alert(event.data.message+$(this).text());
     }
     
@@ -35,11 +35,10 @@ var App = (function() {
     
     function validateEmail(emailTest) {
         var filter = /^[a-zA-Z0-9]+[a-zA-Z0-9_.-]+[a-zA-Z0-9_-]+@[a-zA-Z0-9]+[a-zA-Z0-9.-]+[a-zA-Z0-9]+.[a-z]{2,4}$/;
-        if (filter.test(emailTest) && emailTest.length < 99) {
+        if (filter.test(emailTest) && emailTest.length < 99)
             return true;
-        } else {
+        else
             return false;
-        }
     }
     
     function validateName(nameTest) {
@@ -47,21 +46,18 @@ var App = (function() {
         var valid = !(filter.test(nameTest));
         
         // at least 2 letters
-        if (nameTest.length < 2) {
+        if (nameTest.length < 2)
             valid = false;
-        }
         
         // at least one letter character
         filter = /[a-z]/gi;
-        if (!(filter.test(nameTest))) {
+        if (!(filter.test(nameTest)))
             valid = false;
-        }
         
         // no extra spaces
         filter = /\s{2,}/g;
-        if (filter.test(nameTest)) {
+        if (filter.test(nameTest))
             valid = false;
-        }
         
         return valid;
     }
@@ -79,9 +75,8 @@ var App = (function() {
             x = cookies[i].substr(0, cookies[i].indexOf('='));
             y = cookies[i].substr(cookies[i].indexOf('=')+1);
             x = x.replace(/^\s+|\s+$/g,'');
-            if (x == cookieName) {
+            if (x == cookieName)
                 cookieValue = unescape(y);
-            }
         }
         return cookieValue;
     }
@@ -103,9 +98,9 @@ var App = (function() {
         // create public vars / functions here, inside the closure
         
         init : function() {
-            
+
             setEvents();
-        }   
+        }
     };
 
 }());
